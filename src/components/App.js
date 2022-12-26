54ab57e5145cddc008f7814212ac3d36abce5573
Sajan Jain <sajan.jain@ocean.ibm.com>
2022-12-26 16:05:06 +0530

    Added Search Bar and Button with Keydown function

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 
 class App extends Component {
+    state = { artistQuery: '' };
+
+    updateArtistQuery = event => {
+        this.setState({ artistQuery: event.target.value });
+    }
+
+    handleKeyPress = event => {
+        if (event.key === 'Enter') {
+            this.searchArtist();
+        }
+    }
+
+    searchArtist = () => {
+        console.log('this.state', this.state);
+    }
 
     render() {
         return (
-            <div>React App Template</div>
+            <div>
+                <h2>Music Master</h2>
+                <input
+                    onChange={this.updateArtistQuery}
+                    onKeyPress={this.handleKeyPress}
+                    placeholder="Search for an Artist" />
+                <button onClick={this.searchArtist}>Search</button>
+            </div>
         )
     }
 }
